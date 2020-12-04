558bbe124660f49c6a4a3f9dcefc9bd82c698b68
Nigel <nigel.vigan@gmail.com>
2020-12-03 17:55:07 -0800

    third commit

---
+++ IS51Test2.py
@@ -0,0 +1,30 @@
+
+
+
+
+"""
+The program will display information about student grades on a final exam based off of a input text file
+containing the grades.
+To create the program, a main() function will be used to initialize the application.
+A calculate_percent_above_average will also be used to calculate
+the percentage of grades that are above the average grade.
+Then, all the grades in the input text file will be read using the open() method.
+From there, the grades will be assigned to a list of integers.
+After the program runs, the information that will be printed to the user include the number of grades,
+the average grade, and the percentage of grades that are above the average grade.
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
