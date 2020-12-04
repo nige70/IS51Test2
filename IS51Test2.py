1e8755d23aba2abfa07a09c72dc4593b920f4339
Nigel <nigel.vigan@gmail.com>
2020-12-03 17:51:00 -0800

    exam-end

--- IS51Test2.py
+++ IS51Test2.py
@@ -33,17 +33,24 @@ calculate_percent_above_average
 
 main
 """
+## Analyze grades on a final exam
 
+def main():
+  infile = open("Final.txt", 'r')
+  grades = [line.rstrip() for line in infile]
+  infile.close()
+  for i in range(len(grades)):
+    grades[i] = int(grades[i])
 
+def calculate_percent_above_average():
+  average = sum(grades) / len(grades)
+  num = 0
+  for grade in grades:
+    if grade > average:
+      num += 1
+  print("Number of grades:", len(grades))
+  print("Average grade:", average)
+  print("Percentage of grades above average: {0:.2f}%".format(100 * num / len(grades)))
 
-
-
-
-
-
-
-
-
-
-
+main()
 
